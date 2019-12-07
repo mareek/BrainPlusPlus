22964ad0aafa79758bec770a05f7b65484cea2ee
Matthieu Mourisson <mareek@gmail.com>
2019-12-07 12:33:48 +0100

    Addedjump table initialisation for loop management

--- Interpreter.cpp
+++ Interpreter.cpp
@@ -1,1 +1,38 @@
+#include <stack>
 #include "Interpreter.h"
+
+Interpreter::Interpreter(string program)
+{
+	m_program = program;
+	m_tape = new Tape();
+	m_jumpTable = new int[program.size()];
+}
+
+Interpreter::~Interpreter()
+{
+	delete m_tape;
+	delete m_jumpTable;
+}
+
+void Interpreter::InitJumpTable()
+{
+	auto openingBracketPositions = new std::stack<int>();
+	for (int i = 0; i < m_program.length; i++)
+	{
+		if (m_program.at(i) == '[')
+		{
+			openingBracketPositions->push(i);
+		}
+		else if (m_program.at(i) == ']')
+		{
+			int startBracketPosition = openingBracketPositions->top();
+			openingBracketPositions->pop();
+			m_jumpTable[startBracketPosition] = i;
+			m_jumpTable[i] = startBracketPosition;
+		}
+	}
+}
+
+void Interpreter::Execute()
+{
+}
--- Interpreter.h
+++ Interpreter.h
@@ -1,7 +1,21 @@
 #pragma once
+#include <string>
+#include "Tape.h"
+using namespace std;
+
 class Interpreter
 {
 private:
-	char* m_program;
+	string m_program;
+	int m_position;
+	Tape* m_tape;
+	int* m_jumpTable;
+
+	void InitJumpTable();
+public:
+	Interpreter(string program);
+	~Interpreter();
+
+	void Execute();
 };
 
