1fd56d76fe36575a26a4f9153b01e5f93bcb4d96
Matthieu Mourisson <mareek@gmail.com>
2019-12-11 08:39:39 +0100

    Handle file passed on the command line

--- BrainPlusPlus.cpp
+++ BrainPlusPlus.cpp
@@ -1,8 +1,26 @@
 #include "Interpreter.h"
+#include <fstream>
 
-int main()
+int main(int argc, char* argv[])
 {
-	string outputFortyTwoProgram = "[-]>[-]<>+++++++[<+++++++>-]<+++.--.";
-	auto interpreter = new Interpreter(outputFortyTwoProgram);
+	string program;
+	if (argc > 1)
+	{
+		auto programPath = argv[1];
+		ifstream iFileStream(programPath);
+		if (!iFileStream.good())
+		{
+			throw std::runtime_error("File not found");
+		}
+		program.assign((istreambuf_iterator<char>(iFileStream)), istreambuf_iterator<char>());
+	}
+	else
+	{
+		const string outputFortyTwoProgram = "[-]>[-]<>+++++++[<+++++++>-]<+++.--.";
+		program = outputFortyTwoProgram;
+	}
+
+	Interpreter* interpreter = new Interpreter(program);
 	interpreter->Execute();
+	delete interpreter;
 }
